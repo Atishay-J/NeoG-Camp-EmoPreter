3c441f01c9eb4fe2a0ea87c5116cd6b2aa1e8704
Atishay Jain <hritikjain707@gmail.com>
2020-12-14 15:53:09 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+
+let emojiData = {
+  "😄": "Grinning Face with Smiling Eyes",
+  "😂": "Face with Tears of Joy",
+  "😍": "Smiling Face with Heart-Eyes",
+  "😩": "Weary Face",
+  "😡": "Angry Face ",
+  "😉": "Winking Face",
+};
+
+let emojis = Object.keys(emojiData);
 
 function App() {
+  const [meaning, setMeaning] = useState("Start Typing to Know Meaning");
+  let emojiMeans;
+  /////////////////////////////////////////////////// Getting Emoji meaning from Database
+
+  let getMeaning = (input) => {
+    let meaningFromDB = emojiData[input];
+
+    if (meaningFromDB) {
+      return (emojiMeans = meaningFromDB);
+    }
+    return (emojiMeans =
+      "Sorry Currently this is Not available in our Database");
+  };
+
+  ///////////////////////////////////////////////////////// Taking Input
+
+  let inputHandler = (event) => {
+    let input = event.target.value;
+    console.log(input);
+    getMeaning(input);
+    setMeaning(emojiMeans);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <div className="header">
+          <h1 className="logo">
+            Emo<span className="logo_col">Preter</span>
+          </h1>
+        </div>
+        <h3 className="title">Type any Emoji to find out its real meaning</h3>
+        <input
+          className="input"
+          onChange={inputHandler}
+          placeholder="Type 😀 here"
+        />
+        <h5 className="meaning">{meaning}</h5>
+        <h5 className="text">These are the Emojis in our Database</h5>
+        <h6 className="text-sm">(Click on them to know there meaning)</h6>
+        <div className="emojiBox">
+          {emojis.map((emoji) => {
+            return (
+              <span
+                className="emoji"
+                onClick={(event) => {
+                  getMeaning(event.target.innerHTML);
+                  setMeaning(emojiMeans);
+                }}
+              >
+                {emoji}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+      <footer className="footer">
+        <h4 class="foot-text">Made with ❤ by Atishay</h4>
+        <h5 class="copy">© 2020</h5>
+      </footer>
     </div>
   );
 }
